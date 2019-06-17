338e6c5048f82865dbf01b6b1d2a9205ea324f2d
Koen Goedemondt <koen@ubiqu.com>
2019-06-17 21:59:08 +0200

    not yet implemented single container

--- gui.py
+++ gui.py
@@ -1,13 +1,17 @@
-from flask import Flask, url_for
+from flask import Flask
 from flask import render_template
 
 app = Flask(__name__, instance_relative_config=True)
 
 
 @app.route('/')
-def hello_world():
-    context = {}
-    return render_template('index.html', context=context)
+def index():
+    context = dict()
+    context['motor_gauges'] = [i for i in range(1, 5)]
+    context['graphs'] = [{'title': 'Acceleration', 'label': 'acceleration', 'width': '90%', 'height': 'auto'},
+                         {'title': 'Gyro', 'label': 'gyro', 'width': '90%', 'height': 'auto'},
+                         {'title': 'Sensors', 'label': 'sensors', 'width': '90%', 'height': 'auto'}]
+    return render_template('base.html', **context)
 
 
 @app.route('/data')
